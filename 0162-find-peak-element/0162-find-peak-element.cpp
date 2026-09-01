090e9b7574aeb28939342fff11a08304452bdb25
Udayan Sanmotra <sanmotraudayan28@gmail.com>
2026-09-02 02:49:38 +0530

    Time: 0 ms (100%), Space: 12.7 MB (12.08%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Function to find a peak element using binary search
+    int findPeakElement(vector<int>& nums) {
+        // Set left and right bounds
+        int low = 0, high = nums.size() - 1;
+
+        // Binary search loop
+        while (low < high) {
+            // Find mid point
+            int mid = (low + high) / 2;
+
+            // If mid element is greater than next
+            if (nums[mid] > nums[mid + 1]) {
+                // Move to left half
+                high = mid;
+            } else {
+                // Move to right half
+                low = mid + 1;
+            }
+        }
+
+        // Return peak index
+        return low;
+    }
+};
